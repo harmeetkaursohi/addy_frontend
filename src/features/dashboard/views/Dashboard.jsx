0b0ed1587fe21fd5218ed10de8fb622f171540e2
Harmeet Kaur <harmeetkaursohi95@gmail.com>
2023-09-21 20:10:50 +0530

    minor fix.

--- src/features/dashboard/views/Dashboard.jsx
+++ src/features/dashboard/views/Dashboard.jsx
@@ -266,7 +266,6 @@ const Dashboard = () => {
                                                              xmlns="http://www.w3.org/2000/svg" onClick={()=>setFacebookDropDown(!facebookDropDown)}>
                                                             <path id="Icon"
                                                                   d="M13 1L7.70711 6.29289C7.31658 6.68342 6.68342 6.68342 6.29289 6.29289L1 1"
-                                                                  d="M13 1L7.70711 6.29289C7.31658 6.68342 6.68342 6.68342 6.29289 6.29289L1 1"
                                                                   stroke="#5F6D7E" strokeWidth="1.67"
                                                                   strokeLinecap="round"/>
                                                         </svg>
