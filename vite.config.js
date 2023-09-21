396e48713194a2a33cfe4b710011ee790ad71542
Harmeet Kaur <harmeetkaursohi95@gmail.com>
2023-09-22 01:12:33 +0530

    minor fix.

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        // basicSsl()
-    ],
     server: {
         host:true,
         port:5173,
-        watch: {
-            usePolling: false
-        }
     },
     build: {
         watch: false
