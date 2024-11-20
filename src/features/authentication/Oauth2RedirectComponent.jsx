68f94afeba76fcd75f769b1ff2eed0adea44ffbe
Pankaj Yadav <pankaj.yadav@ultivic.com>
2024-11-20 13:41:07 +0530

    logs removed

--- src/features/authentication/Oauth2RedirectComponent.jsx
+++ src/features/authentication/Oauth2RedirectComponent.jsx
@@ -7,27 +7,20 @@ const Oauth2RedirectComponent = () => {
 
     const getUrlParameter = (name) => {
         name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-        console.log("name======>",name)
         const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
         const results = regex.exec(location.search);
-        console.log("results======>",results)
-        console.log("if results==null======>",decodeURIComponent(results[1].replace(/\+/g, ' ')))
         return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-
     };
 
     const token = getUrlParameter('token');
     const error = getUrlParameter('error');
-
-    console.log("token======>",token)
-    console.log("error======>",error)
     useEffect(() => {
         if (token) {
             localStorage.setItem("token", token);
-            // window.location.href = "/dashboard"
+            window.location.href = "/dashboard"
         } else {
             localStorage.setItem("errorInOAuth","true")
-            // window.location.href = "/login"
+            window.location.href = "/login"
         }
     }, []);
 
