72865fe0201c6160ed159cc73d1d110048a3c32f
Pankaj Yadav <pankaj.yadav@ultivic.com>
2024-12-31 16:05:40 +0530

    openai secret key removed from env file

--- src/utils/RTKQueryUtils.jsx
+++ src/utils/RTKQueryUtils.jsx
@@ -25,7 +25,7 @@ export const getAuthorizationHeader = () => {
 
 export const getOpenAIAuthHeader = () => {
     return {
-        'Authorization': `Bearer ${import.meta.env.VITE_APP_OPEN_API_KEY}`
+        'Authorization': `Bearer ${import.meta.env.VITE_APP_OPEN_AI_API_KEY}`
     }
 }
 
